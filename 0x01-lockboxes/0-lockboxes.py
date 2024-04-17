7059ed3cb24c31d74685bfa1730781410fced8ac
root <anas.baidouri@gmail.com>
2024-04-17 23:22:48 +0000

    updt

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -1,24 +1,17 @@
 #!/usr/bin/python3
 """Function to check if all lockboxes can be opened"""
 
+
 def canUnlockAll(boxes):
-    n = len(boxes)
-    if n == 0:
-        return False
+    unlocked = set()
 
-    unlocked = set([0])  # Start with the key to the first box
-    for box_id, box in enumerate(boxes):
-        if box_id in unlocked:
-            unlocked.update(box)  # Add keys from the current box to unlocked
-
-    return len(unlocked) == n
-
-# Test cases
-boxes1 = [[1], [2], [3], [4], []]
-print(canUnlockAll(boxes1))  # Output: True
-
-boxes2 = [[1, 4, 6], [2], [0, 4, 1], [5, 6, 2], [3], [4, 1], [6]]
-print(canUnlockAll(boxes2))  # Output: True
-
-boxes3 = [[1, 4], [2], [0, 4, 1], [3], [], [4, 1], [5, 6]]
-print(canUnlockAll(boxes3))  # Output: False
+    for box_id in range(len(boxes) - 1, -1, -1):
+        box = boxes[box_id]
+        if len(box) == 0 or box_id in unlocked:
+            unlocked.add(box_id)
+        for key in box:
+            if key < len(boxes):
+                unlocked.add(key)
+        if len(unlocked) == len(boxes):
+            return True
+    return False
